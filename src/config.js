d4959b71005eb8c674c372fc6db6b80531228c30
Lau <laurentiucozma12@gmail.com>
2025-02-27 21:03:47 +0200

    Pinged your deployment. You successfully connected to MongoDB!

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-require("dotenv").config(); // Load environment variables
+require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.MONGO_URI; // Load from .env
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
