c938de83fa6ef5b03a8be42730c53a03d9b733e2
Lau <laurentiucozma12@gmail.com>
2025-02-27 22:46:20 +0200

    login register works

--- src/config.js
+++ src/config.js
@@ -1,29 +1,28 @@
-require("dotenv").config();
+const mongoose = require("mongoose");
+const connect = mongoose.connect("mongodb://localhost:27017/Disertatie");
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.MONGO_URI;
+// Check database connected or not
+connect
+  .then(() => {
+    console.log("Database Connected Successfully");
+  })
+  .catch(() => {
+    console.log("Database cannot be Connected");
+  });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+// Create Schema
+const Loginschema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
   },
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
